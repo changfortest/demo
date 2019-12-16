d1a5d7bde4708446472c184f74897bb6a62cd580
yanghua <changfor.aliyun.com>
2019-12-16 10:51:36 +0800

    dev_yanhhua

--- src/main/java/com/example/demo/controller/Login.java
+++ src/main/java/com/example/demo/controller/Login.java
@@ -18,9 +18,7 @@ import java.util.HashMap;
 @RestController
 @RequestMapping(path = "/user")
 public class Login {
-
     //dev_yanghua做的改动
-    //A做的改动
 
     @Autowired
     private RedisTemplate redisTemplate;
